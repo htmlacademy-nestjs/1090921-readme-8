ee336d935998a5c92016e3187bb49c98250e14c7
Victoria <onexsan@gmail.com>
2024-11-25 21:44:38 +0300

    Добавляет маршруты в AuthenticationController

--- project/libs/user/authentication/src/authentication-module/authentication.service.ts
+++ project/libs/user/authentication/src/authentication-module/authentication.service.ts
@@ -4,9 +4,7 @@ import {
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
-
 import { BlogUserRepository, BlogUserEntity } from '@project/blog-user';
-
 import { CreateUserDto } from '../dto/create-user.dto';
 import {
   AUTH_USER_EXISTS,
@@ -19,7 +17,7 @@ import { LoginUserDto } from '../dto/login-user.dto';
 export class AuthenticationService {
   constructor(private readonly blogUserRepository: BlogUserRepository) {}
 
-  public async register(dto: CreateUserDto) {
+  public async register(dto: CreateUserDto): Promise<BlogUserEntity> {
     const { email, name, password, avatar } = dto;
 
     const blogUser = {
@@ -28,40 +26,33 @@ export class AuthenticationService {
       avatar,
       passwordHash: '',
     };
-
     const existUser = await this.blogUserRepository.findByEmail(email);
-
     if (existUser) {
       throw new ConflictException(AUTH_USER_EXISTS);
     }
-
     const userEntity = await new BlogUserEntity(blogUser).setPassword(password);
 
-    return this.blogUserRepository.save(userEntity);
+    this.blogUserRepository.save(userEntity);
+
+    return userEntity;
   }
 
   public async verifyUser(dto: LoginUserDto) {
     const { email, password } = dto;
     const existUser = await this.blogUserRepository.findByEmail(email);
-
     if (!existUser) {
       throw new NotFoundException(AUTH_USER_NOT_FOUND);
     }
-
     if (!(await existUser.comparePassword(password))) {
       throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
     }
-
     return existUser;
   }
-
   public async getUser(id: string) {
     const user = await this.blogUserRepository.findById(id);
-
     if (!user) {
       throw new NotFoundException(AUTH_USER_NOT_FOUND);
     }
-
     return user;
   }
 }
