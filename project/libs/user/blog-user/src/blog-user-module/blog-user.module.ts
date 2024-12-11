368e5ac6c43709288fd769ed56f3c3145363281b
Victoria <onexsan@gmail.com>
2024-12-11 12:39:27 +0400

    Добавляет BaseMongoRepository и обновляет BlogUserRepository

--- project/libs/user/blog-user/src/blog-user-module/blog-user.module.ts
+++ project/libs/user/blog-user/src/blog-user-module/blog-user.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { BlogUserRepository } from './blog-user.repository';
 import { BlogUserFactory } from './blog-user.factory';
 import { BlogUserController } from './blog-user.controller';
+import { BlogUserModel, BlogUserSchema } from './blog-user.model';
 
 @Module({
+  imports: [MongooseModule.forFeature([
+    { name: BlogUserModel.name, schema: BlogUserSchema }
+  ])],
   providers: [BlogUserRepository, BlogUserFactory],
   controllers: [BlogUserController],
   exports: [BlogUserRepository],
